b7a9894418e4ae1390c041a863c39857de468c17
Kimberly Ivie <ktbt02@yahoo.com>
2019-11-07 22:41:50 -0600

    area classs determines area of a cirle based on radius. also created circle class. trying to commit more

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,4 +1,22 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+
+        int r;
+        int A;
+        double pi;
+        Scanner input;
+
+        pi = 3.14;
+
+        input = new Scanner(System.in);
+        System.out.println("What is the circle's raduis?");
+        r = input.nextInt();
+        input.close();
+
+        System.out.println("The area of a circle with a radius of " + r + " is " + (2 * pi * r));
+    }
 }
