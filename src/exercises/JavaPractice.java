53f0e90f425a8367d41ae1a0a8726d165714920e
Kimberly Ivie <ktbt02@yahoo.com>
2019-11-10 11:24:23 -0600

    made my array practice file. made my array of integers and have it looping through them.

---
+++ src/exercises/JavaPractice.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class JavaPractice {
+    public static void main(String[] args){
+        int[] arrayNums = {1, 1, 2, 3, 5, 8};
+
+        for (int i = 0; i < arrayNums.length; i++){
+            System.out.println(arrayNums[i]);
+        }
+    }
+}
