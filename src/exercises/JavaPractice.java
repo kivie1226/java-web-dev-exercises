b17328cebd270a5ae9eeefbcb5050d6cba4e5ea0
Kimberly Ivie <ktbt02@yahoo.com>
2019-11-17 13:42:13 -0600

    finsihed the array practice for chapter 3

--- src/exercises/JavaPractice.java
+++ src/exercises/JavaPractice.java
@@ -2,14 +2,28 @@ package exercises;
 
 public class JavaPractice {
     public static void main(String[] args){
-        int[] arrayNums = {1, 1, 2, 3, 5, 8};
+        int[] arrayNumbs = {1, 1, 2, 3, 5, 8}; //making my array
+        String saying = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in" +
+                " a house. I will not eat them with a mouse.";
 
-        for (int i = 0; i < arrayNums.length; i++){
-            System.out.println(arrayNums[i]);
-        } for (int i = 0; i< arrayNums.length; i++){
-            if (arrayNums[i] % 2 != 0) {
-                System.out.println(arrayNums[i]);
+        for (int i = 0; i < arrayNumbs.length; i++){ //this loop will print off all integers in arrayNumbs
+            System.out.println(arrayNumbs[i]);
+        }
+        System.out.println("\n");
+        for (int i = 0; i< arrayNumbs.length; i++){ //this loop prints off only odd integers in arrayNumbs
+            if (arrayNumbs[i] % 2 != 0) {
+                System.out.println(arrayNumbs[i]);
             }
+        }
+        System.out.println("\n");
+        String[] arrOfSaying = saying.split(" "); //Splitting our saying at each space
+        for (String a: arrOfSaying){
+            System.out.println(a);
+        }
+        System.out.println("\n");
+        String[] arrOfSayingSentences = saying.split("\\. "); //Splitting our saying at each sentence
+        for (String b: arrOfSayingSentences){
+            System.out.println(b);
         }
     }
 }
